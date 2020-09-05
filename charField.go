4251539cee96ffc209cf71a67b2321fdc6a2d761
Bekmamat <akmataliev95@gmail.com>
2020-09-05 19:13:28 +0300

    new fields have been added

--- charField.go
+++ charField.go
@@ -1,5 +1,7 @@
 package forms
 
+import "regexp"
+
 type charField struct {
 	field
 }
@@ -32,6 +34,48 @@ func CharField(name string, required bool, minLen, maxLen int, vs ...Validator) 
 	var validator = func(val interface{}) error {
 		if s := val.(string); len(s) < minLen || len(s) > maxLen {
 			return T("length should be between %v and %v", minLen, maxLen)
+		}
+		return nil
+	}
+	var f = &charField{
+		field{
+			name:     name,
+			required: required,
+			ftype:    "String",
+			vs:       []Validator{validator},
+		},
+	}
+	f.vs = append(f.vs, vs...)
+	return f
+}
+
+var emailRE = regexp.MustCompile("^\\S{4,20}@\\S{2,15}\\.\\S{2,6}$")
+
+func EmailField(name string, required bool, vs ...Validator) Field {
+	var validator = func(val interface{}) error {
+		if !emailRE.Match([]byte(val.(string))) {
+			return T("invalid email")
+		}
+		return nil
+	}
+	var f = &charField{
+		field{
+			name:     name,
+			required: required,
+			ftype:    "String",
+			vs:       []Validator{validator},
+		},
+	}
+	f.vs = append(f.vs, vs...)
+	return f
+}
+
+var uuidRE = regexp.MustCompile("^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$")
+
+func UUIDField(name string, required bool, vs ...Validator) Field {
+	var validator = func(val interface{}) error {
+		if !uuidRE.Match([]byte(val.(string))) {
+			return T("invalid uuid")
 		}
 		return nil
 	}

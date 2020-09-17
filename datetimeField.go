0acf6e1734b109b80deee64cf6c8a96d5e1dc1a7
bakmataliev <b.akmataliev@molbulak.com>
2020-09-17 16:05:12 +0300

    duration field

--- datetimeField.go
+++ datetimeField.go
@@ -1,6 +1,9 @@
 package forms
 
-import "time"
+import (
+	"strconv"
+	"time"
+)
 
 type datetimeField struct {
 	field
@@ -53,6 +56,53 @@ func DateTimeField(name string, required bool, vs ...Validator) Field {
 			name:     name,
 			required: required,
 			ftype:    "String or Number",
+			vs:       vs,
+		},
+	}
+}
+
+type durationField struct {
+	field
+}
+
+func (f *durationField) Assign(val interface{}) error {
+	switch value := val.(type) {
+	case string:
+		var converted, err = strconv.ParseFloat(value, 64)
+		if err != nil {
+			return typeMismatchError
+		}
+		f.value = time.Duration(converted)
+	case float64:
+		f.value = time.Duration(value)
+	default:
+		return typeMismatchError
+	}
+	return nil
+}
+
+func (f *durationField) Validate(val interface{}) []string {
+	var errors = make([]string, 0)
+	if !f.required && val == nil {
+		return errors
+	}
+	if f.required && val == nil {
+		errors = append(errors, t(fieldRequired))
+		return errors
+	}
+	if f.Assign(val) != nil {
+		errors = append(errors, t(typeMismatch, f.ftype))
+		return errors
+	}
+	return f.runValidators(errors)
+}
+
+func DurationField(name string, required bool, vs ...Validator) Field {
+	return &numberField{
+		field{
+			name:     name,
+			required: required,
+			ftype:    "Number",
 			vs:       vs,
 		},
 	}
